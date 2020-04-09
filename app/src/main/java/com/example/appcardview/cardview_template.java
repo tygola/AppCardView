c9fc50a33c930ce7efb81f25fae8911942ca09fb
tygola <tweedleti@gmail.com>
2020-04-09 17:49:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/appcardview/cardview_template.java
@@ -0,0 +1,14 @@
+package com.example.appcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardview_template extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview_template);
+    }
+}
